9133a7b08a0573521539450969ce1a7482cd0ad6
Eric Bezanson <eric.bezanson@outlook.com>
2025-01-07 15:30:35 -0400

    WIP re-org

--- cmd/main.go
+++ cmd/main.go
@@ -4,29 +4,40 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 
-	"tictacgo/game"
-	"tictacgo/ws"
+	"tictacgo/pkg/game"
+	"tictacgo/pkg/ws"
 
 	"golang.org/x/net/websocket"
 )
 
 func main() {
+	// Get the current working directory
+	basePath, err := os.Getwd()
+	if err != nil {
+		log.Fatalf("Failed to get current working directory: %v", err)
+	}
+
+	// Construct the absolute path to the "web/templates" directory
+	templatesPath := filepath.Join(basePath, "web", "templates")
+
 	// Initialize the Tic-Tac-Toe game
 	ticTacToe := game.NewGame()
 
-	// Serve static files from the "static" directory
-	fs := http.FileServer(http.Dir("./static"))
+	// Serve static files from the "web/templates" directory
+	fs := http.FileServer(http.Dir(templatesPath))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
 	// Home page route
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "static/index.html")
+		http.ServeFile(w, r, filepath.Join(templatesPath, "index.html"))
 	})
 
 	// Lobby route
 	http.HandleFunc("/lobby", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "static/lobby.html")
+		http.ServeFile(w, r, filepath.Join(templatesPath, "lobby.html"))
 	})
 
 	// WebSocket route
