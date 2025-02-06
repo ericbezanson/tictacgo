d737cc95382028d20e7f9e176935b7010c12e0ff
Eric Bezanson <eric.bezanson@outlook.com>
2025-02-06 16:03:23 -0400

    move routes out of main.go and into its own package

---
+++ pkg/routes/routes.go
@@ -0,0 +1,27 @@
+package routes
+
+import (
+	"net/http"
+	"tictacgo/api/handlers"
+	"tictacgo/pkg/lobby"
+
+	"golang.org/x/net/websocket"
+)
+
+// SetupRoutes configures all HTTP routes for the server
+func SetupRoutes() {
+	// Serve static files from "web/templates"
+	http.Handle("/", http.FileServer(http.Dir("./web/templates")))
+
+	// Serve static files from "web/static"
+	fs := http.FileServer(http.Dir("./web/static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	// Handle lobby routes
+	http.HandleFunc("/create-lobby", lobby.CreateLobby)
+	http.HandleFunc("/lobbies", lobby.HandleLobbies)
+	http.HandleFunc("/lobby/", lobby.ServeLobby)
+
+	// WebSocket handler
+	http.Handle("/ws", websocket.Handler(handlers.HandleWebSocket))
+}
